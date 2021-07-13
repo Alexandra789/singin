d101a8f7fa164c6236816a44abdce37ea9c8e997
unknown <zakovinko99@gmail.com>
2021-07-13 11:26:29 +0300

    refactor code

--- script.js
+++ script.js
@@ -1,69 +1,75 @@
-
-// console.log("APSDKFAPSDFPAOSDIFPADSAFSD");
-// @import "tailwindcss/base";
-// @import "tailwindcss/components";
-// @import "tailwindcss/utilities";
-// $('body').on('click', '.password-control', function(){
-//     if ($('#password-input').attr('type') == 'password'){
-//         $(this).addClass('view');
-//         $('#password-input').attr('type', 'text');
-//     } else {
-//         $(this).removeClass('view');
-//         $('#password-input').attr('type', 'password');
-//     }
-//     return false;
-// });
-// function validateEmail(email) {
-//    return  reg.terst(email);
-// }
-// let regEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-// let regPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
-function validateEmail(email){
-if(!/[a-zA-Z\d]{8,}/.test(email)){
-  return "Email should have at least 8 symbols";
-}
-if(!/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
-  return "Incorrect email";
-}
-return "";
-}
-
-function validatePassword(password) {
-if(!/[a-zA-Z\d]{8,}/.test(password)){
-  return "Password should have at least 8 symbols";
-}
-if(!/[A-Z]+/.test(password)) {
-     return "Password should have at least one uppercase letter";
-}
-if(!/[0-9]+/.test(password)) {
-     return "Password should have at least number";
-}
-if(!/[0-9]+/.test(password)) {
-     return "Password should have at least number";
-}
-return "";
-
-}
-
-
 let formButton = document.querySelector('.form__button');
 let inputEmail = document.querySelector('.email-input');
 let inputPassword = document.querySelector('.password-input');
 let errorEmail = document.querySelector('.error-email');
 let errorPassword = document.querySelector('.error-password');
+let passwordControl = document.querySelector('.password-control');
+
+inputEmail.onblur = ()=> {
+  if(inputEmail.value === ''){
+    inputEmail.classList.add('error');
+    errorEmail.innerText = 'Please enter your email';
+  }
+  else{
+    inputEmail.classList.remove('error');
+    errorEmail.innerText = '';
+  }
+}
+
+inputPassword.onblur = ()=> {
+  if(inputPassword.value === ''){
+    inputPassword.classList.add('error');
+    errorPassword.innerText = 'Please enter your password';
+  }
+  else{
+    inputPassword.classList.remove('error');
+    errorPassword.innerText = '';
+  }
+}
+
+passwordControl.onclick = () => {
+  if(passwordControl.classList.contains('show')){
+    passwordControl.classList.remove('show');
+    passwordControl.classList.add('hidden');
+    inputPassword.setAttribute('type', 'password');
+  }
+  else{
+    passwordControl.classList.remove('hidden');
+    passwordControl.classList.add('show');
+    inputPassword.setAttribute('type', 'text');
+  }
+}
+
+function validateEmail(email){
+  if(!/[a-zA-Z\d]{8,}/.test(email)){
+    return "Email should have at least 8 symbols";
+  }
+  if(!/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
+    return "Incorrect email";
+  }
+  return "";
+}
+
+function validatePassword(password) {
+  if (!/[a-zA-Z\d]{8,}/.test(password)) {
+    return "Password should have at least 8 symbols";
+  }
+  if (!/[A-Z]+/.test(password)) {
+    return "Password should have at least one uppercase letter";
+  }
+  if (!/[0-9]+/.test(password)) {
+    return "Password should have at least number";
+  }
+  if (!/[0-9]+/.test(password)) {
+    return "Password should have at least number";
+  }
+  return "";
+}
 
 formButton.addEventListener('click', function (e) {
-e.preventDefault()
-// errorEmail.innerHtml = '';
-// errorPassword.innerHtml ='';
-let email = document.querySelector('.email-input').value;
-let password = document.querySelector('.password-input').value;
-console.log(errorPassword);
-console.log(errorPassword.innerText +'llll');
-// console.log(regEmail.test(email))
-// console.log(regPassword.test(password));
-  console.log(validatePassword(password));
-  // let er =
+  e.preventDefault()
+  let email = document.querySelector('.email-input').value;
+  let password = document.querySelector('.password-input').value;
   if(validateEmail(email) !== ""){
     errorEmail.innerText = validateEmail(email);
     inputEmail.classList.add('error');
@@ -72,8 +78,8 @@ console.log(errorPassword.innerText +'llll');
     errorEmail.innerText = '';
     inputEmail.classList.remove('error');
   }
-    if(validatePassword(password) !== ""){
-      errorPassword.innerText = validatePassword(password);
+  if(validatePassword(password) !== ""){
+    errorPassword.innerText = validatePassword(password);
     inputPassword.classList.add('error');
   }
 
@@ -81,6 +87,4 @@ console.log(errorPassword.innerText +'llll');
     errorPassword.innerText = '';
     inputPassword.classList.remove('error');
   }
-
-
 })
